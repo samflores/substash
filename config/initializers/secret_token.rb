81c2d481db3ca55caeac142e9ec7f16a13f20039
Samuel Flores <me@samflor.es>
2011-11-11 12:03:52 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Substash::Application.config.secret_token = 'aaadabb13eb96d71d5c93e618bd873c2125d19d14db81bf1497d15ddd1fcab9e7afdb4c91b4dfce6c7810fc2a9451fcd4793800b75a87df17816e4573459efc7'
